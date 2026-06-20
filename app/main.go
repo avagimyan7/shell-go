9caf542f4f1f8797eda5f67fb2461e162db40152
Ashot <a.avagimian@a-m-a.com>
2026-06-20 14:22:32 +0400

    Stage pn5: the exit builtin [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -14,10 +15,24 @@ func main() {
 		fmt.Print("$ ")
 
 		input, err := reader.ReadString('\n')
-		command := strings.TrimRight(input, "\r\n")
+		line := strings.TrimRight(input, "\r\n")
 
-		if command != "" {
-			fmt.Printf("%s: command not found\n", command)
+		if line != "" {
+			fields := strings.Fields(line)
+			name, args := fields[0], fields[1:]
+
+			switch name {
+			case "exit":
+				code := 0
+				if len(args) > 0 {
+					if parsed, perr := strconv.Atoi(args[0]); perr == nil {
+						code = parsed
+					}
+				}
+				os.Exit(code)
+			default:
+				fmt.Printf("%s: command not found\n", name)
+			}
 		}
 
 		if err != nil { // io.EOF (Ctrl+D) or a read error: leave the REPL
