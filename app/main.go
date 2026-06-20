92a961fd6112169e1151466b4735cce60464e9c3
Ashot <a.avagimian@a-m-a.com>
2026-06-20 14:20:16 +0400

    Stage cz2: handle invalid commands (command not found + REPL loop) [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Print("$ ")
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("$ ")
+
+		input, err := reader.ReadString('\n')
+		command := strings.TrimRight(input, "\r\n")
+
+		if command != "" {
+			fmt.Printf("%s: command not found\n", command)
+		}
+
+		if err != nil { // io.EOF (Ctrl+D) or a read error: leave the REPL
+			break
+		}
+	}
 }
